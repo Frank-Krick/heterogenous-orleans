b4df78854782fb33e1cfe1321924873dd1eab6d7
Frank Krick <frank.krick@gmail.com>
2019-05-04 18:39:18 -0400

    Build script fixes and initial silo

--- src/Silo/Program.cs
+++ src/Silo/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Statistics;
 
 namespace Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateClusterHost();
+            host.RunAsync().Wait();
+        }
+
+        private static IHost CreateClusterHost()
+        {
+            var hostBuilder = new HostBuilder()
+                .UseOrleans(siloBuilder =>
+                {
+                    var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+                    connectionStringBuilder.Host = "orleans-postgres";
+                    connectionStringBuilder.Database = "orleans";
+                    connectionStringBuilder.Username = "orleans";
+                    connectionStringBuilder.Password = "rewgpionevofrrrfgjjj";
+                    siloBuilder.Configure((Action<ClusterOptions>) (options =>
+                    {
+                      options.ClusterId = "heterogenous-test-cluster";
+                      options.ServiceId = "heterogenous-test-cluster";
+                    }));
+                    siloBuilder.ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000);
+                    siloBuilder.AddAdoNetGrainStorage("grain-storage", options =>
+                    {
+                        options.Invariant = "Npgsql";
+                        options.ConnectionString = connectionStringBuilder.ConnectionString;
+                    });
+
+                    siloBuilder.UseAdoNetClustering(options =>
+                    {
+                        options.Invariant = "Npgsql";
+                        options.ConnectionString = connectionStringBuilder.ConnectionString;
+                    });
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        siloBuilder.UseLinuxEnvironmentStatistics();
+                    }
+                });
+
+            return hostBuilder.Build();
         }
     }
 }
