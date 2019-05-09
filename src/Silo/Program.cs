bc0abf64c68deb21979217723aa77c9b53c10008
Frank Krick <frank.krick@gmail.com>
2019-05-08 22:27:15 -0400

    Base silo package

--- src/Silo/Program.cs
+++ src/Silo/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using Microsoft.Extensions.Hosting;
-using Npgsql;
-using Orleans;
-using Orleans.Configuration;
-using Orleans.Hosting;
-using Orleans.Statistics;
+﻿using Microsoft.Extensions.Hosting;
+using SiloBase;
 
 namespace Silo
 {
@@ -20,40 +14,7 @@ namespace Silo
         private static IHost CreateClusterHost()
         {
             var hostBuilder = new HostBuilder()
-                .UseOrleans(siloBuilder =>
-                {
-                    var connectionStringBuilder = new NpgsqlConnectionStringBuilder
-                    {
-                        Host = "orleans-postgres",
-                        Database = "orleans",
-                        Username = "orleans",
-                        Password = "rewgpionevofrrrfgjjj"
-                    };
-                    siloBuilder.Configure((Action<ClusterOptions>) (options =>
-                    {
-                      options.ClusterId = "heterogenous-test-cluster";
-                      options.ServiceId = "heterogenous-test-cluster";
-                    }));
-                    siloBuilder.ConfigureEndpoints(11111, 30000);
-                    siloBuilder.AddAdoNetGrainStorage("grain-storage", options =>
-                    {
-                        options.Invariant = "Npgsql";
-                        options.ConnectionString = connectionStringBuilder.ConnectionString;
-                    });
-
-                    siloBuilder.UseAdoNetClustering(options =>
-                    {
-                        options.Invariant = "Npgsql";
-                        options.ConnectionString = connectionStringBuilder.ConnectionString;
-                    });
-
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                    {
-                        siloBuilder.UseLinuxEnvironmentStatistics();
-                    }
-
-                    siloBuilder.UseDashboard(options => { options.Port = 1337; });
-                });
+                .UseOrleans(siloBuilder => { siloBuilder.ConfigureSilo(); });
 
             return hostBuilder.Build();
         }
---
+++ src/SiloBase/ConfigExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Npgsql;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Statistics;
+
+namespace SiloBase
+{
+    public static class ConfigExtensions
+    {
+        public static ISiloBuilder ConfigureSilo(this ISiloBuilder builder)
+        {
+                    var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+                    {
+                        Host = "orleans-postgres",
+                        Database = "orleans",
+                        Username = "orleans",
+                        Password = "rewgpionevofrrrfgjjj"
+                    };
+                    builder.Configure((Action<ClusterOptions>) (options =>
+                    {
+                      options.ClusterId = "heterogenous-test-cluster";
+                      options.ServiceId = "heterogenous-test-cluster";
+                    }));
+                    builder.ConfigureEndpoints(11111, 30000);
+                    builder.AddAdoNetGrainStorage("grain-storage", options =>
+                    {
+                        options.Invariant = "Npgsql";
+                        options.ConnectionString = connectionStringBuilder.ConnectionString;
+                    });
+
+                    builder.UseAdoNetClustering(options =>
+                    {
+                        options.Invariant = "Npgsql";
+                        options.ConnectionString = connectionStringBuilder.ConnectionString;
+                    });
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        builder.UseLinuxEnvironmentStatistics();
+                    }
+
+                    builder.UseDashboard(options => { options.Port = 1337; });
+
+            return builder;
+        }
+    }
+}
